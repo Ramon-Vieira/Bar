a845ae751f8c14ed1bb5e1ab0fbcdc979779cd5d
Ramon-Vieira <ramon.s.v@hotmail.com>
2019-05-28 14:24:49 -0300

    atualizaçao index

--- views/site/index.php
+++ views/site/index.php
@@ -2,50 +2,44 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+//$this->title = 'My Yii Application';
+$this->title = 'Bar Sábado Letivo';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Bar Sábado Letivo!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Se for dirigir não beba! Mas se for beber me chame!</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Beba com moderação</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Cerveja Artesanal</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>A cerveja é uma bebida muito antiga e que acompanha a humanidade desde as primeiras civilizações. Desde o seu surgimento na Suméria há mais de 8.000 anos atrás, vários povos continuaram produzindo cerveja, cada um do seu jeito, conforme os ingredientes e a tecnologia da época permitiam. Com o passar dos anos, isso foi dando origem a diversos tipos – denominados tecnicamente de “estilos” – diferentes de cerveja, que nos acompanham até hoje..</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Cerveja Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Cachaça Envelhecida</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Para esta categoria, a bebida fica, no mínimo 50% dela durante pelo menos 1 ano em tonéis de no máximo 700 litros. Por ser um tonel menor e por ficar retido por mais tempo, as cachaças envelhecidas acabam apresentando alterações mais evidentes na sua cor, aroma e paladar. Dentro delas, existem duas classificações. As cachaças Premium são 100% envelhecidas de um a três anos. Já a Extra-Premium, é 100% envelhecida no tempo mínimo de três anos.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Cachaça Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Tequila</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Tequila é um inferno de uma bebida alcoólica destilada feita da agave-azul (uma planta semelhante a um abacaxi gigante e só se desenvolve em terrenos de solo vulcânico e clima árido), primariamente na região da cidade de Tequila no estado mexicano de Jalisco, a 65 quilómetros a nordeste de Guadalajara.
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                O solo vulcânico vermelho da região circundante é particularmente propício ao crescimento do agave-azul, e mais de 300 milhões de plantas são colhidas todo ano. Agave tequilana cresce diferentemente dependendo da região. Agaves-azuis plantadas em áreas altas são maiores e mais doces em aroma e sabor. Agaves das áreas mais baixas tem um sabor e fragrância mais herbáceos.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Tequila Extensions &raquo;</a></p>
             </div>
         </div>
 
